42e0a3a8bf963110a85ac1ed7aa37e7af46bb544
Yoav Tsook <yoavt@wix.com>
2025-08-30 23:31:37 +0300

    Update service worker cache (trigger deployment test)
    - This commit triggers a new deployment to test if GitHub Pages base path is working
    - Both build:mock and build:prod scripts already have GITHUB_PAGES=true set
    - Should fix resource loading on https://yoavtsook2806.github.io/trip-bundle/

--- packages/trip-bundle-pwa/dev-dist/sw.js
+++ packages/trip-bundle-pwa/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-fce877f4'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.uumt1q7v4n8"
+    "revision": "0.6vu6d93odio"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
